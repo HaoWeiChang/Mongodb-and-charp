6a9f19e49a88894f1056914eb6b405decbce1cac
Haowei_Chang <37842778+HaoWeiChang@users.noreply.github.com>
2021-04-18 21:50:57 +0800

    Create LibraryDocument.cs

---
+++ ConsoleApp1/ConsoleApp1/LibraryDocument.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Bson;
+
+namespace ConsoleApp1
+{
+    class LibraryDocument
+    {
+        public string _id { get; set; }
+        public string book { get; set; }
+        public float price { get; set; }
+        public string[] authors { get; set; }
+        public Borrower borrower { get; set; }
+    }
+    public class Borrower
+    {
+        public string name { get; set; }
+        public BsonDateTime timestamp { get; set; }
+
+    }
+}
