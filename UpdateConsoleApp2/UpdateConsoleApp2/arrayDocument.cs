cc5ad91ea17c10a30dcc3e05927d10bfe33f06b3
Haowei_Chang <37842778+HaoWeiChang@users.noreply.github.com>
2021-04-22 07:41:14 +0800

    Add files via upload

---
+++ UpdateConsoleApp2/UpdateConsoleApp2/arrayDocument.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UpdateConsoleApp2
+{
+    class arrayDocument
+    {
+        public string _id { get; set; }
+        public int[] list { get; set; }
+    }
+}
