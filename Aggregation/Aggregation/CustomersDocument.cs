a1d35eb3392fdbf73461b46cb4df7752b7915e1f
Haowei_Chang <37842778+HaoWeiChang@users.noreply.github.com>
2021-04-22 11:22:36 +0800

    Add files via upload

---
+++ Aggregation/Aggregation/CustomersDocument.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aggregation
+{
+    class CustomersDocument
+    {
+        public string city { get; set; }
+        public string district { get; set; }
+        public int age { get; set; }
+    }
+}
