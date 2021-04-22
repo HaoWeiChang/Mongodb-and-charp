d8d25afb168ecb5f699aa0cd73bbac8f3d6372bb
Haowei_Chang <37842778+HaoWeiChang@users.noreply.github.com>
2021-04-22 10:58:15 +0800

    Add files via upload

---
+++ ConsoleApp1/coordinationDocumet.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Driver.GeoJsonObjectModel;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class coordinationDocument
+    {
+        public string _id { get; set; }
+        public string name { get; set; }
+        public GeoJsonPoint<GeoJson2DGeographicCoordinates>  location { get; set; }       
+    }
+    
+   
+}
