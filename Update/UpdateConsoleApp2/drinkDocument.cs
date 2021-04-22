4948826a2475c44bedada3d023d74da5350d7709
Haowei_Chang <37842778+HaoWeiChang@users.noreply.github.com>
2021-04-22 11:38:50 +0800

    Add files via upload

---
+++ Update/UpdateConsoleApp2/drinkDocument.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UpdateConsoleApp2
+{
+    class drinkDocument
+    {
+        public string _id { get; set; }
+        public string product { get; set; }
+        public string type { get; set; }
+        public Price price { get; set; }
+        public int sold { get; set; }
+        public Log[] log { get; set; }
+    }
+    public class Price
+    {
+        public int M { get; set; }
+        public int L { get; set; }
+
+    }
+    public class Log
+    {
+        public DateTime time { get; set; }
+        public string size { get; set; }
+    }
+}
---
+++ Update/UpdateConsoleApp2/driversDocument.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UpdateConsoleApp2
+{
+    class driversDocument
+    {
+        public string _id { get; set; }
+        public string group { get; set; }
+        public string status { get; set; }
+        public Seatinfo seatinfo { get; set; }
+        public double[] location { get; set; }
+        public DateTime lastrModified { get; set; }
+
+
+        public class Seatinfo
+        {
+            public int capacity { get; set; }
+            public int remaining { get; set; }
+            public int riding { get; set; }
+
+        }
+    }
+}
